9f8c5543a371dff9c7922946211b66889833584a
Daniel Estupiñán <danielestupinan0802@gmail.com>
2023-01-08 11:27:05 -0500

    Feat: settings for theme colors

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
+import { ThemeContext, useMode } from "./theme";
+import { CssBaseline, ThemeProvider } from "@mui/material";
+import Header from "./pages/global/Header";
 
 function App() {
-  return (
-    <div className="App">
 
-    </div>
+  const [theme, colorMode] = useMode();
+
+  return (
+    <ThemeContext.Provider value={colorMode}>
+      <ThemeProvider theme={theme}>
+        {/* Reset css */}
+        <CssBaseline />
+
+        <div className="App" >
+          <main className="content">
+            <Header />
+
+          </main>
+        </div>
+
+      </ThemeProvider>
+    </ThemeContext.Provider >
+
   );
 }
 
