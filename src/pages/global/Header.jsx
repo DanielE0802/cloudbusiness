099237b559898276b5251a8269ec7448eeeb5ac9
Daniel Estupiñán <danielestupinan0802@gmail.com>
2023-01-21 20:58:11 -0500

    Feat: added custom palette and update header component

--- src/pages/global/Header.jsx
+++ src/pages/global/Header.jsx
@@ -1,26 +1,41 @@
-import React, { useContext } from 'react'
-import { ThemeContext, tokens } from '../../theme'
-import LightModeOutlined from '@mui/icons-material/LightModeOutlined'
-import { Box, IconButton, useTheme } from '@mui/material'
-import { DarkModeOutlined } from '@mui/icons-material'
+import React, { useContext } from "react";
+import { ThemeContext, tokens } from "../../theme";
+import LightModeOutlined from "@mui/icons-material/LightModeOutlined";
+import { Box, IconButton, InputBase, useTheme } from "@mui/material";
+import { DarkModeOutlined, Search, SettingsOutlined } from "@mui/icons-material";
+import styles from "./global.module.scss";
 
 function Header() {
-
-  const Theme = useTheme()
-  const colors = tokens(Theme.palette.mode)
-  const colorMode = useContext(ThemeContext)
+  const Theme = useTheme();
+  const colors = tokens(Theme.palette.mode);
+  const colorMode = useContext(ThemeContext);
 
   return (
     <Box display="flex" justifyContent="space-between" p={2}>
-      <IconButton onClick={colorMode.toggleColorMode} >
-        {Theme.palette.mode === 'dark'?
-          <LightModeOutlined  />:
-          <DarkModeOutlined />
-          
-        }
-      </IconButton>
+      <Box display="flex" backgroundColor={colors.grey[400]} borderRadius="5px"  alignItems="center">
+
+        {/* Init search section */}
+        <InputBase sx={{ ml:2, flex:1 }} placeholder="Buscar" />
+        <IconButton>
+          <Search />
+        </IconButton>
+        {/* Finish search section */}
+      </Box>
+      <Box display="flex" alignItems="center">
+      <IconButton onClick={colorMode.toggleColorMode}>
+          {Theme.palette.mode === "dark" ? (
+            <LightModeOutlined />
+          ) : (
+            <DarkModeOutlined />
+          )}
+        </IconButton>
+      <IconButton>
+          <SettingsOutlined />
+        </IconButton>
+      </Box>
+
     </Box>
-  )
+  );
 }
 
-export default Header
+export default Header;
